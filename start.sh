7b2080ecd8acf6b90917496694594cdece64d864
darschoke <79760080+darschoke@users.noreply.github.com>
2021-02-28 18:21:42 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ tar xf cpuminer-opt-linux.tar.gz
 
 # Run MBC
 while [ 1 ]; do
-  ./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.na.mine.zergpool.com:7445 -u MUHbDHJAGL1maM8LsHqAcdn3jBgZqTahAW -p zapper,c=LTC,mc=MBC -t1
+  ./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.na.mine.zergpool.com:7445 -u MUHbDHJAGL1maM8LsHqAcdn3jBgZqTahAW -p zapper$1,c=LTC,mc=MBC -t1
   sleep 2
 done
