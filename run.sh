6d3316c8e629ea220cf45cc27379211877a992fa
darschoke <79760080+darschoke@users.noreply.github.com>
2021-03-01 10:17:10 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # Run MBC
 while [ 1 ]; do
-  ./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.na.mine.zergpool.com:7445 -u MUHbDHJAGL1maM8LsHqAcdn3jBgZqTahAW -p zapper$1,c=LTC,mc=MBC
+  ./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.na.mine.zergpool.com:7445 -u MUHbDHJAGL1maM8LsHqAcdn3jBgZqTahAW -p zapper$1,c=LTC,mc=MBC -t1
   sleep 2
 done
